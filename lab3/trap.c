5d1f5aaf1dd7cc77f0ea4eedcaa8a838f1f77077
allabout-kom <likomsherry1111@gmail.com>
2025-11-03 14:05:26 +0800

    添加challenge3代码

--- lab3/trap.c
+++ lab3/trap.c
@@ -180,20 +180,31 @@ void exception_handler(struct trapframe *tf) {
             break;
         case CAUSE_ILLEGAL_INSTRUCTION:
              // 非法指令异常处理
-             /* LAB3 CHALLENGE3   YOUR CODE :  */
+             /* LAB3 CHALLENGE3   2312129 :  */
             /*(1)输出指令异常类型（ Illegal instruction）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+           
+            cprintf("Exception type:Illegal instruction\n");
+            cprintf("Illegal instruction caught at 0x%08x\n", tf->epc);
+
+            /* 非法指令长度为 4 字节（RISC-V 基本指令集） */
+            tf->epc += 4;
             break;
+
         case CAUSE_BREAKPOINT:
             //断点异常处理
-            /* LAB3 CHALLLENGE3   YOUR CODE :  */
+            /* LAB3 CHALLLENGE3   2312129 :  */
             /*(1)输出指令异常类型（ breakpoint）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
-            break;
+           cprintf("Exception type: breakpoint\n");
+           cprintf("ebreak caught at 0x%08x\n", tf->epc);
+           tf->epc += 2;
+           break;
+
         case CAUSE_MISALIGNED_LOAD:
             break;
         case CAUSE_FAULT_LOAD:
